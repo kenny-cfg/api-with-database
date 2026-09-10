1db05fbe0b0eb01eeb34894cb5020f6952e83512
Kenny Hung (he) <kenny.cfg@outlook.com>
2026-09-10 19:17:32 +0100

    GET /user working

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS social;
+CREATE DATABASE social;
+
+USE social;
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(255) UNIQUE,
+  age INT
+);
--- index.js
+++ index.js
@@ -9,11 +9,21 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
-})
+});
 
 const api = express();
 const port = 3000;
 api.use(express.json());
+
+api.get('/user', async (req, res) => {
+  const [rows] = await pool.promise().query('select * from users');
+  const result = rows.map(row => ({
+    name: row.name,
+    email: row.email,
+    age: row.age
+  }));
+  res.json(result);
+})
 
 api.listen(port, () => {
   console.log(`Listening on port ${port}`);
