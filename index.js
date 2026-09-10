9e7ee5b6a6846909a8339927268561a36e78102d
Kenny Hung (he) <kenny.cfg@outlook.com>
2026-09-10 18:53:58 +0100

    Connection pool

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const express = require('express');
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'password123',
+  database: 'social',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+})
+
 const api = express();
 const port = 3000;
 api.use(express.json());
